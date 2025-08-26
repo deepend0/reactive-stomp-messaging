0736421eed18a1f5027031e6cc4bf5a266438ac5
Orkun Akile <orkun.akile@gmail.com>
2025-08-27 01:53:01 +0200

    Broker Inbound And Message Endpoint Destination Regex Matching

--- base/runtime/src/main/java/com/github/deepend0/reactivestomp/messaging/messagehandler/MessagingMessageHandler.java
+++ base/runtime/src/main/java/com/github/deepend0/reactivestomp/messaging/messagehandler/MessagingMessageHandler.java
@@ -2,6 +2,8 @@ package com.github.deepend0.reactivestomp.messaging.messagehandler;
 
 import com.github.deepend0.reactivestomp.messaging.model.Message;
 import com.github.deepend0.reactivestomp.messaging.model.SendMessage;
+import com.github.deepend0.reactivestomp.messaging.model.SubscribeMessage;
+import com.github.deepend0.reactivestomp.messaging.model.UnsubscribeMessage;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.reactive.messaging.MutinyEmitter;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -14,16 +16,20 @@ import java.util.Optional;
 
 @ApplicationScoped
 public class MessagingMessageHandler {
-    private final Optional<List<String>> messageEndpointPrefixes;
+    private final Optional<List<String>> brokerRegexes;
+
+    private final Optional<List<String>> messageEndpointRegexes;
 
     private final MutinyEmitter<Message> brokerInboundEmitter;
 
     private final MutinyEmitter<SendMessage> messageEndpointInboundEmitter;
 
-    public MessagingMessageHandler(@ConfigProperty(name = "reactive-stomp.messaging.message-endpoint.paths") Optional<List<String>> messageEndpointPrefixes,
+    public MessagingMessageHandler(@ConfigProperty(name = "reactive-stomp.messaging.broker.regex") Optional<List<String>> brokerRegexes,
+                                   @ConfigProperty(name = "reactive-stomp.messaging.message-endpoint.regex") Optional<List<String>> messageEndpointRegexes,
                                    @Channel("brokerInbound") MutinyEmitter<Message> brokerInboundEmitter,
                                    @Channel("messageEndpointInbound") MutinyEmitter<SendMessage> messageEndpointInboundEmitter) {
-        this.messageEndpointPrefixes = messageEndpointPrefixes;
+        this.brokerRegexes = brokerRegexes;
+        this.messageEndpointRegexes = messageEndpointRegexes;
         this.brokerInboundEmitter = brokerInboundEmitter;
         this.messageEndpointInboundEmitter = messageEndpointInboundEmitter;
     }
@@ -31,8 +37,24 @@ public class MessagingMessageHandler {
     @Incoming("messagingInbound")
     public Uni<Void> handle(Message message) {
         if (message instanceof SendMessage sendMessage) {
-            if (messageEndpointPrefixes.stream().flatMap(List::stream).anyMatch(prefix -> sendMessage.getDestination().startsWith(prefix))) {
+            if (messageEndpointRegexes.stream().flatMap(List::stream).anyMatch(regex -> sendMessage.getDestination().matches(regex))) {
                 return messageEndpointInboundEmitter.send(sendMessage);
+            } else if (brokerRegexes.stream().flatMap(List::stream).anyMatch(regex -> sendMessage.getDestination().matches(regex))) {
+                return brokerInboundEmitter.send(message);
+            } else {
+                return Uni.createFrom().voidItem();
+            }
+        } else if (message instanceof SubscribeMessage subscribeMessage) {
+            if (brokerRegexes.stream().flatMap(List::stream).anyMatch(regex -> subscribeMessage.getDestination().matches(regex))) {
+                return brokerInboundEmitter.send(message);
+            } else {
+                return Uni.createFrom().voidItem();
+            }
+        } else if (message instanceof UnsubscribeMessage unsubscribeMessage) {
+            if (brokerRegexes.stream().flatMap(List::stream).anyMatch(regex -> unsubscribeMessage.getDestination().matches(regex))) {
+                return brokerInboundEmitter.send(message);
+            } else {
+                return Uni.createFrom().voidItem();
             }
         }
         return brokerInboundEmitter.send(message);
